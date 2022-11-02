5aec8265164e86ed5db1d908806ae9d3dd8d6b73
NITRO-AN515-56\Nam <namyahoo2003@gmail.com>
2022-11-02 17:12:34 +0700

    Add new project food

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, { useState } from "react";
 import "./App.css";
 import FormRegister from "./components/Form";
+import {Routes, Route, Navigate} from 'react-router-dom'
 
 export default function App() {
 	return (
-		<FormRegister />
+		<Routes>
+			<Route path="/" element= {<Navigate to='/homework-form'/>}/>
+			<Route path="/homework-form" element={<FormRegister />}/>
+		</Routes>
 	);
 }
